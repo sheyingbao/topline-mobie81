17460d3e4613bbe6cabebe06dddd2aa0096cf7a5
syb <youremail@example.com>
2019-09-07 11:07:07 +0800

    修改目录结构

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './store/store'
+import store from './router/index'
 
 Vue.config.productionTip = false
 
